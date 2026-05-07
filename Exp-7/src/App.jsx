6de87092baecd0f02b4b1cb75d4cbe2275a0cb88
Venkatesh Singh <venkateshsingh455@gmail.com>
2026-05-08 00:21:23 +0530

    react

--- Exp-7/src/App.jsx
+++ Exp-7/src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Student from './components/Student'
 import Greeting from './components/Greeting'
+import Name from './components/Name'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
       <Student name={'Rahul Sharma'} course={'Computer Science'} marks={85}/><br/>
       <Student name={'Anita Verma'} course={'Information Technology'} marks={92}/><br/>
       <Student name={'Rohan Gupta'} course={'Electronics'} marks={78}/><br/>
+      <Name/>
     </>
   )
 }
---
+++ Exp-7/src/components/Name.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Name() {
+  return (
+    <div>
+        <h3>VENKATESH SINGH</h3>
+        <h3>2503201001313</h3>
+        <h3>CSE-26</h3>
+    </div>
+  )
+}
+
+export default Name
