b45aaad4a50eab76b0ab17f239f47e5bf67d9bac
Venkatesh Singh <venkateshsingh455@gmail.com>
2026-05-14 23:22:20 +0530

    react

---
+++ Exp-8/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Counter from "./Counter";
+
+function App() {
+    return (
+        <div>
+            <Counter />
+            <div className="STUDENT-INFO">
+              <p><strong>NAME:</strong>Venkatesh singh</p>
+              <p><strong>roll no:</strong>2503201001313</p>
+              <p><strong>class:</strong>cse-26</p>
+            </div>
+        </div>
+    );
+}
+
+export default App
