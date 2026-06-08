f3cfbadf78079c2da1ddd27402601f10c25f2977
VenkateshSingh-blip <venkateshsingh455@gmail.com>
2026-06-08 19:47:11 +0530

    Deleted

--- Experiment_12/src/main/java/com/example/employeeapi/repository/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.employeeapi.repository;
-
-import com.example.employeeapi.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
